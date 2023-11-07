a1a5e2a551c680d43c1f26a36c406d01bf34b626
Homework1 <kalinina_en@hotmail.com>
2023-11-07 22:15:45 +0300

    formatted the table

--- src/main/java/MultiplicationTable.java
+++ src/main/java/MultiplicationTable.java
@@ -1,10 +1,12 @@
 public class MultiplicationTable {
     public static void main(String[] args) {
 
-        for (int i = 2; i <= 10; i++) {
-            for (int j = 2; j <= 10; j++) {
-                System.out.println(i + "*" + j + "=" + i * j);
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.print(i + " * " + j + " = " + i * j + "\t" + "\t");
             }
+            System.out.println();
         }
     }
 }
+
